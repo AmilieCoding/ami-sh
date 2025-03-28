ab66bd721ff232d35b348b7f4821a12e08ed64da
AmilieCoding <amilie@allthingslinux.org>
2025-03-28 07:01:29 +0000

    Added autocompletion support.

--- main/utilities/executor.c
+++ main/utilities/executor.c
@@ -1,18 +1,25 @@
 #include "shell.h"
 
 // -> Actually handling the commands :3
-void execute_command(char **args) {
-    if (args[0] == NULL) return;
-
-    // -> Error handling :3
+int execute_command(char **args) {
     pid_t pid = fork();
+    
     if (pid == 0) {
+        // -> If PID == 0 then it is child process, if it fails to run
+        // -> It will then print an error message with perror. It then
+        // -> Proceeds to exit.
         execvp(args[0], args);
-        perror("exec failed");
-        exit(1);
+        perror("ami-sh");
+        exit(EXIT_FAILURE);
     } else if (pid > 0) {
-        wait(NULL);
+        // -> Ensure prompt is on a new line (I hope!)
+        int status;
+        waitpid(pid, &status, 0);
+        printf("\n");
     } else {
-        perror("fork failed");
+        // -> Incase forking PID fails :cri:
+        perror("ami-sh");
     }
+
+    return 1;
 }
